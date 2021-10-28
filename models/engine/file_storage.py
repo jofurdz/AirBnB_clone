b9a128ad11dd19f09749ac6a0882802f8b2e4e28
Niqualis <NicholasJWallace@protonmail.com>
2021-10-28 10:42:47 -0500

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """class FileStorage"""
 
-
+#import
 import json
-
 
 class FileStorage:
     """class FileStoarge"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """return object attribute dict"""
+    
+        return __objects
+    
+    def new(self, obj):

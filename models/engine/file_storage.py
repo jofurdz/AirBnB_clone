33943dce270f67ce9a91683cefc3330fc6d4a08a
Niqualis <NicholasJWallace@protonmail.com>
2021-10-28 12:13:30 -0500

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """class FileStorage"""
 
+
+from models.base_model import BaseModel
+
+
 #import
 import json
 
@@ -14,5 +18,27 @@ class FileStorage:
         """return object attribute dict"""
     
         return __objects
-    
+        
     def new(self, obj):
+        """ create new dict item"""
+        
+        key = obj.__class__.__name__ + "obj.id"
+
+    def save(self):
+        """serializes to JSON file"""
+
+        newDict = {}
+        for key, value in self.__objects.items():
+            newDict[key] = value.to_dict()
+
+        with open(self.__file_path, "w") as fd:
+            json.dump(newDict, fd)
+
+    def reload(self)
+        """deserializes JSON file"""
+
+        pass
+
+
+
+        

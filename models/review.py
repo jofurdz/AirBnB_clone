eb4f66974d7e18461a3ddb412d01e5288b0588f7
Niqualis <NicholasJWallace@protonmail.com>
2021-10-29 11:30:23 -0500

    update

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' Model defines class Review'''
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    '''Review class definition'''
+
+    place_id = ""
+    user_id = ""
+    test = ""
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' all class to inherit BaseModel'''
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    ''' defining State inherited from BaseModel'''
+
+    name = ""

90baa2279faab72f1733c5f2aae46d8d3ffedcda
Niqualis <NicholasJWallace@protonmail.com>
2021-11-01 19:01:05 -0500

    tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Amenity class test"""
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Testing of the Amenity Class"""
+
+    def setUp(self):
+        '''Turn Amenity into Amenity variable'''
+
+        
